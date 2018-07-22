6beda4fa70edba57e9cb6296a7b44e44ed174294
Amit Biswas <biswas1992@hotmail.com>
2018-07-22 13:43:56 -0400

    Initial commit with some modofications

---
+++ application/views/page/about.php
@@ -0,0 +1,54 @@
+<div style="min-height: 430px;" class="lo-container">
+<h3 style="text-align: center;">ABOUT ME</h3>
+<br><br>
+<section>
+<h3>Introduction</h3>
+
+<p>Learning for me is always a very exciting venture.I believe that knowledge gained is something no one can take away from me once it is acquired.   In reviewing the course package at BrainStation, I was most excited to see the focus on iOS development.  IOS development is my main passion and I would like to acquire as many new skills through the program.  I spoke in depth about the course components with one of the learning advisors, Mr. Mitch.</p>
+<p>I also gave him a good idea about my programming experience and we spoke about Object Oriented Programming.  He encouraged me to apply to the program and agreed that I would benefit greatly from the courses available. Another aspect of BrainStation’s course design that excited me was the interactive live classes.  It’s always valuable to be able to get direct feedback and ask questions as they arise.  It is also imperative to get direct consultation from professionals in the industry that</p>
+<p>can share real world proficiency. Overall, I really enjoy the way the entire program is constructed and the benefits I will reap after its completion.</p>
+<section>
+<h3>Notable Work</h3>
+<h4>Thalamus</h4>
+<p>
+Nicole co-founded
+<a href='https://thalamus.ai'>Thalamus</a>
+in 2017, with the aim of solving meaningful, real-world problems through the use of artificial intelligence. In its first year, Thalamus has created solutions for
+<a href="http://kaidogroup.com">Kaido</a>,
+<a href="http://redbrickbills.co.uk">RedBrick Bills</a>,
+and
+<a href="http://www.bigwideworld.co/">Big Wide World</a>, and been shortlisted for New Business of the Year.
+</p>
+<p>
+
+<a href="https://reachora.com">Reachora</a>
+- a platform for medical publishers - and developing an artificial intelligence algorithm for use during diagnostic heart-surgery, which if successful will result in improved outcomes for 150,000 patients worldwide each year.
+</p>
+<h4>Meducation</h4>
+<p>
+Nicole joined
+<a href='https://meducation.net'>Meducation</a>
+after their seed round as Head of Product. She led Meducation's development from a small UK site to an internationally respected platform used by 15% of medical students worldwide.
+</p>
+<p>Meducation provides thousands of free learning resources to medical students, supplementing their formal university learning, and providing access to high-quality teaching material for students from all over the world. It is 100% free.</p>
+<h4>Exercism</h4>
+<p>
+<a href='https://v2.exercism.io'>Exercism</a>
+was founded in 2012 by Katrina Owen as a platform to help budding software developers improve their coding skills through mentorship and practice.
+</p>
+<p>Thalamus took a 30% stake in the company in 2017. Nicole led a full product redevelopment, designing the platform to be friendly, welcoming and accessible to anyone regardless of geography. Exercism is now used by 150,000 software developers worldwide. It is 100% free.</p>
+<h4>Project Management</h4>
+<p>Before entering the startup world, Nicole wanted to get experience of bigger businesses so worked as a project manager on a number of projects, including an £82m infrastructure project for NFU Mutual, a company-wide software upgrade project at Wragge and Co, and a relocation project for Lloyds Banking Group.</p>
+<p>While completing these projects, Nicole qualified as both a PRINCE2 Practitioner and an Accredited Project Management under the APM Foundation.</p>
+</section>
+<section>
+<h3>Get in touch</h3>
+<p>
+You can reach Nicole on
+<a href='https://twitter.com/n_chalmers'>Twitter</a>
+or
+<a href='mailto:nicole@thalamus.ai'>by email</a>.
+</p>
+</section>
+</div>
+<br><br>
